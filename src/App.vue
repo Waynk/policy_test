ae381d12deac5c390ebc4dcce91a158b4d02509f
Waynk <ww930304@gmail.com>
2026-03-24 11:46:13 +0800

    save before adding vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="stylus">
